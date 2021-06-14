a020fe405d294ce07b9be365c6aded70a317d89a
Rodrigo Hjort <rodrigo.hjort@serpro.gov.br>
2021-06-14 18:45:57 -0300

    conversão incremental dos arquivos MP3 em WAV

--- 21-converter-mp3-para-wav-5s.sh
+++ 21-converter-mp3-para-wav-5s.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 TAMANHO=5 # cada corte terá essa duração (em segundos)
 
-#rm -rf wav${TAMANHO}s; mkdir wav${TAMANHO}s
+direntrada="mp3"
 dirsaida="wav"
-#dirsaida="wav${TAMANHO}s"
 if [ ! -d $dirsaida ]; then mkdir $dirsaida; fi
 
-#for entrada in `find mp3/ -name "*.mp3" | head -1`
-for entrada in `find -L mp3/ -name "*.mp3"`
+for entrada in `find -L $direntrada/ -name "*.mp3" -type f`
 do
-  saida=`echo $entrada | sed 's/mp3/wav/g'`
-  dirsaida=`dirname $saida`
+  arquivo=$(basename $entrada)
+  pasta=$(dirname $entrada)
+  nome="${arquivo/.mp3/}"
+  
+  saida="`echo $pasta | sed 's/'$direntrada'/'$dirsaida'/'`/$nome"
+  #saida=`echo $entrada | sed 's/mp3/wav/g'`
+  #dirsaida=`dirname $saida`
   echo "[$entrada] -> [$saida]"
-  #if [ ! -d $dirsaida ]; then mkdir $dirsaida; fi
+
+  # se primeiro corte existe, pular para próximo arquivo
+  if [ -f ${saida}-1.wav ]
+  then
+    continue
+  fi
 
   # extrair a duração do áudio e calcular a quantidade de cortes necessários
   duracao=`ffprobe -i $entrada -show_format -v quiet | sed -n 's/^duration=\([0-9]\+\).*$/\1/p' 2> /dev/null`
@@ -26,7 +34,8 @@ do
   do
     let inicio=(corte-1)*TAMANHO
     let termino=inicio+TAMANHO
-    saidacorte=`echo $saida | sed -e 's/^wav/wav'$TAMANHO's/' -e 's/.wav$/-'$corte'.wav/'`
+    saidacorte="$saida-$corte.wav"
+    #saidacorte=`echo $saida | sed -e 's/^wav/wav'$TAMANHO's/' -e 's/.wav$/-'$corte'.wav/'`
 
     # se for o primeiro corte, cria o diretório (se não existente)
     if [ $corte -eq 1 ]
